007c21131e453faa2d59252575351f48ed91907c
MD RAJ <rajmd1800@gmail.com>
2025-09-26 20:19:05 +0530

    live coding with jalish

---
+++ Day2/PreviousExercise/newfile.js
@@ -0,0 +1,53 @@
+// // Q-1 check palindrome
+
+// let message = "raj";
+// let str=""
+
+// for(let index = message.length-1 ; index>=0 ;index--){
+//     str=str+message[index];//jar
+// }
+
+// console.log(str);
+
+// if(message==str){
+//     console.log("palindrome");
+// }
+// else{
+//     console.log("not palindrome");
+// }
+
+// // let char1 = "a";
+// // let char2 = "b";
+
+// // // let char3 = char1.concat(char2) ;
+// // let char3 = char1+char2;
+
+
+// let msg = "jalish";
+// let arr =[];
+// for(val of msg){
+//     // console.log(val);
+//     arr.push(val);
+// }
+
+// console.log(arr);
+
+
+let person = {
+    name : "jalish",
+    age:25,
+    color:"fair",
+};
+
+// person.name="ram";
+let arr1 =[];
+for(let key in person){
+    arr1.push(person[key]);
+}
+
+console.log(arr1);
+
+
+
+
+// live coding done with jalish
